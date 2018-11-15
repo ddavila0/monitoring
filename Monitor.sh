ce28a3c63a30b7883f0a9cc8d86e0d3d5ce43d1f
root <root@vocms0850.cern.ch>
2018-11-15 11:07:24 +0100

    New arguments format for Monitor.sh

--- Monitor.sh
+++ Monitor.sh
@@ -2,5 +2,5 @@
 cd /data/srv/ddavila/monitoring
 
 # Monitor the itb pool
-python my_monitor.py vocms0809.cern.ch 1>> log/log.out 2>> log/log.err
+python my_monitor.py itb push 1>> log/itb/log.out 2>> log/itb/log.err
 
