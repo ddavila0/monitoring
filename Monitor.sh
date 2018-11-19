991a7989a53f298c17b1a8b044475deff6428a19
root <root@vocms0850.cern.ch>
2018-11-19 14:35:07 +0100

    calling my_monitor with pool name instead of hostname

--- Monitor.sh
+++ Monitor.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-cd /data/srv/ddavila/monitoring
+cd /data/srv/monitoring/monit
 
 # Monitor the itb pool
 python my_monitor.py itb push 1>> log/itb/log.out 2>> log/itb/log.err
+python my_monitor.py global push 1>> log/global/log.out 2>> log/global/log.err
 
