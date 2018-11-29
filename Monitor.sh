3f02748fcfff4d881c53744ce81d36e342121896
root <root@vocms0850.cern.ch>
2018-11-29 16:13:16 +0100

    Enabling CERN pool monitoring; adding ClientMachine attribute to Startds

--- Monitor.sh
+++ Monitor.sh
@@ -3,5 +3,8 @@ cd /data/srv/monitoring/monit
 
 # Monitor the itb pool
 python my_monitor.py itb push 1>> log/itb/log.out 2>> log/itb/log.err
+# Monitor the global pool
 python my_monitor.py global push 1>> log/global/log.out 2>> log/global/log.err
+# Monitor the cern pool
+python my_monitor.py cern push 1>> log/cern/log.out 2>> log/cern/log.err
 
